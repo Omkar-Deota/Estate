96fcbf65f2d5433339c34f7eac33c8422fbe99db
Omkar-Deota <omkarinshu2003@gmail.com>
2024-08-11 20:25:36 +0530

    url updated

--- Frontend/src/api.tsx
+++ Frontend/src/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000'; 
+const API_URL = "https://estate-backend-kojm.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
