19cf4e3629e4f75dbd555ca225d65030b4a66f72
sandeep25sahu <sandeep.sahu2001@ssipmt.com>
2024-08-31 00:41:28 +0530

    all responsive setted

--- Frontend/src/components/curosals/CardCourasal.tsx
+++ Frontend/src/components/curosals/CardCourasal.tsx
@@ -1,27 +1,18 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
-// import img1 from "./graphics/Screenshot 2024-08-10 193221.png";
-// import img2 from "./graphics/Screenshot 2024-08-10 193229.png";
-// import img3 from "./graphics/Screenshot 2024-08-10 193241.png";
-// import img4 from "./graphics/Screenshot 2024-08-10 193319.png";
-// import img5 from "./graphics/Screenshot 2024-08-10 193352.png";
-// import img6 from "./graphics/Screenshot 2024-08-10 193415.png";
-// import img7 from "./graphics/Screenshot 2024-08-10 193415.png";
-// import img8 from "./graphics/Screenshot 2024-08-10 193547.png";
 
-  const list = [
-    { title: "Villa", img:"src/components/graphics/Screenshot 2024-08-10 193221.png", price: "$100k" },
-    { title: "Tangi",img:"src/components/graphics/Screenshot 2024-08-10 193229.png", price: "$120k" },
-    { title: "Raspberry",img:"src/components/graphics/Screenshot 2024-08-10 193241.png", price: "$130k" },
-    { title: "Liverpool",img:"src/components/graphics/Screenshot 2024-08-10 193319.png", price: "$90k" },
-    { title: "Avodo",img:"src/components/graphics/Screenshot 2024-08-10 193334.png", price: "$76k" },
-    { title: "Limon",img:"src/components/graphics/Screenshot 2024-08-10 193352.png", price: "$130k" },
-    { title: "Mannat",img:"src/components/graphics/Screenshot 2024-08-10 193415.png", price: "$140k" },
-    { title: "Klassy",img:"src/components/graphics/Screenshot 2024-08-10 193519.png", price: "$135k" },
-  ];
+const list = [
+    { title: "Villa", img: "src/components/graphics/Screenshot 2024-08-10 193221.png", price: "$100k" },
+    { title: "Tangi", img: "src/components/graphics/Screenshot 2024-08-10 193229.png", price: "$120k" },
+    { title: "Raspberry", img: "src/components/graphics/Screenshot 2024-08-10 193241.png", price: "$130k" },
+    { title: "Liverpool", img: "src/components/graphics/Screenshot 2024-08-10 193319.png", price: "$90k" },
+    { title: "Avodo", img: "src/components/graphics/Screenshot 2024-08-10 193334.png", price: "$76k" },
+    { title: "Limon", img: "src/components/graphics/Screenshot 2024-08-10 193352.png", price: "$130k" },
+    { title: "Mannat", img: "src/components/graphics/Screenshot 2024-08-10 193415.png", price: "$140k" },
+    { title: "Klassy", img: "src/components/graphics/Screenshot 2024-08-10 193519.png", price: "$135k" },
+];
 
 function CardCourasal() {
     const settings = {
@@ -29,42 +20,57 @@ function CardCourasal() {
         centerMode: true,
         infinite: true,
         centerPadding: "60px",
-        slidesToShow: 3,
-        speed: 500
-      };
-  return (
-<div className="slider-container flex justify-center w-11/12 mx-auto">
-  <div className="flex items-center w-full max-w-full">
-    <Slider {...settings} className="w-full">
-      {list.map((item, index) => (
-        <Card
-          isPressable
-          className="card-hover h-60 bg-slate-100"
-          key={index}
-          shadow="sm"
-          onPress={() => console.log("item pressed")}
-        >
-          <CardBody className="overflow-visible p-0 mx-5">
-            <Image
-              shadow="sm"
-              radius="lg"
-              alt={item.title}
-              className="object-cover h-44 w-96"
-              src={item.img}
-            />
-          </CardBody>
-          <CardFooter className="text-small flex justify-between ">
-            <b className="text-black">{item.title}</b>
-            <p className="text-black">{item.price}</p>
-          </CardFooter>
-        </Card>
-      ))}
-    </Slider>
-  </div>
-</div>
+        slidesToShow: 4,
+        speed: 800,
+        responsive: [
+            {
+                breakpoint: 1024, // For tablets and small laptops
+                settings: {
+                    slidesToShow: 2,
+                    centerPadding: "40px",
+                },
+            },
+            {
+                breakpoint: 768, // For mobile devices
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: "20px",
+                },
+            },
+        ],
+    };
 
-  
-  )
+    return (
+        <div className="slider-container flex justify-center w-full p-8">
+            <div className="flex items-center w-full max-w-full">
+                <Slider {...settings} className="w-full">
+                    {list.map((item, index) => (
+                        <Card
+                            isPressable
+                            className="card-hover bg-slate-100 mx-2 md:mx-6 lg:mx-12"
+                            key={index}
+                            shadow="sm"
+                            onPress={() => console.log("item pressed")}
+                        >
+                            <CardBody className="overflow-visible p-0 flex justify-center items-center">
+                                <Image
+                                    shadow="sm"
+                                    radius="lg"
+                                    alt={item.title}
+                                    className="object-cover w-full md:w-80 lg:w-80 h-44"
+                                    src={item.img}
+                                />
+                            </CardBody>
+                            <CardFooter className="text-small flex justify-between px-4 py-2">
+                                <b className="text-black">{item.title}</b>
+                                <p className="text-black">{item.price}</p>
+                            </CardFooter>
+                        </Card>
+                    ))}
+                </Slider>
+            </div>
+        </div>
+    );
 }
 
-export default CardCourasal
+export default CardCourasal;
--- Frontend/src/components/curosals/Insingth.tsx
+++ Frontend/src/components/curosals/Insingth.tsx
@@ -1,74 +1,81 @@
-
 import Slider from "react-slick";
 import { Card, CardBody, Image } from "@nextui-org/react";
 import { Link } from "@nextui-org/link";
 
 const list = [
-    {title: "Read Latest News",img:"https://cdn-icons-png.flaticon.com/128/2965/2965879.png",url:"https://realty.economictimes.indiatimes.com/" },
-    {title: "Read Articals",img:"https://cdn-icons-png.flaticon.com/128/11693/11693377.png" ,url:"https://www.lexisnexis.in/blogs/property-law-in-india/"},
-    {title: "EMI calcutaror",img:"https://cdn-icons-png.flaticon.com/128/8236/8236106.png",url:"https://emicalculator.net/#google_vignette"},
-    {title: "Area calcutaror",img:"https://cdn-icons-png.flaticon.com/128/5442/5442154.png",url:"https://www.thecalculatorsite.com/conversions/area.php"},
-    {title: "Loan eligibility",img:"https://cdn-icons-png.flaticon.com/128/4599/4599311.png",url:"https://homeloans.sbi/calculators"},
-  ];
+    { title: "Read Latest News", img: "https://cdn-icons-png.flaticon.com/128/2965/2965879.png", url: "https://realty.economictimes.indiatimes.com/" },
+    { title: "Read Articles", img: "https://cdn-icons-png.flaticon.com/128/11693/11693377.png", url: "https://www.lexisnexis.in/blogs/property-law-in-india/" },
+    { title: "EMI Calculator", img: "https://cdn-icons-png.flaticon.com/128/8236/8236106.png", url: "https://emicalculator.net/#google_vignette" },
+    { title: "Area Calculator", img: "https://cdn-icons-png.flaticon.com/128/5442/5442154.png", url: "https://www.thecalculatorsite.com/conversions/area.php" },
+    { title: "Loan Eligibility", img: "https://cdn-icons-png.flaticon.com/128/4599/4599311.png", url: "https://homeloans.sbi/calculators" },
+];
 
 function Insingth() {
-
-    var settings = {
+    const settings = {
         dots: true,
         infinite: false,
         speed: 500,
-        slidesToScroll:1,
-        slidesToShow: 3
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    centerPadding: "20px",
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    centerPadding: "10px",
+                },
+            },
+        ],
     };
 
     return (
-        <div className="bg-slate-300 w-4/5 p-8 ">
-         
-            <text className='text-start'>
-                <p className='text-4xl text-black font-extrabold  ml-6 mb-2'>Insigth & Tools</p>
-                <p className='text-yellow-500 text-xl mb-8'>Get another important imformation </p>
-            </text>
-
-            <div>
+        <div className="bg-slate-300 w-full p-8">
+            <div className="text-start mb-8">
+                <p className="text-4xl text-black font-extrabold ml-6 mb-2">Insight & Tools</p>
+                <p className="text-yellow-500 text-xl mb-8">Get other important information</p>
             </div>
+
             <div className="slider-container">
                 <Slider {...settings}>
-                {list.map((item, index) => (
-        <Card
-          isPressable
-          className="card-hover h-72 w-40 bg-slate-100 ml-12"
-          key={index}
-          shadow="sm"
-        >
-          <CardBody className="overflow-visible p-0 mx-5">
-            <Image
-              className=" h-40 w-40 mx-28"
-              src={item.img}
-            />
-          </CardBody>
-           <b className="text-black">{item.title}</b>
-
-           <Link
-            className="flex  items-center gap-1 "
-            color="foreground"
-            href={item.url}
-           >
-                <button className="bg-pink-600 border-none p-2 text-white rounded-2xl 
-                 hover:bg-pink-800 transition-colors mx-auto">
-          Learn more
-        </button>
-           </Link>
-         
-        </Card>
-      ))}
+                    {list.map((item, index) => (
+                        <Card
+                            isPressable
+                            className="card-hover h-72 w-60 bg-slate-100 mx-2 md:mx-6 lg:mx-12"
+                            key={index}
+                            shadow="sm"
+                        >
+                            <CardBody className="overflow-visible p-0 flex justify-center items-center">
+                                <Image
+                                    className="h-40 w-40 object-cover"
+                                    src={item.img}
+                                    alt={item.title}
+                                />
+                            </CardBody>
+                            <div className="p-4 text-center">
+                                <b className="text-black block">{item.title}</b>
+                                <Link
+                                    className="flex justify-center mt-2"
+                                    color="foreground"
+                                    href={item.url}
+                                >
+                                    <button className="bg-pink-600 border-none p-2 text-white rounded-2xl hover:bg-pink-800 transition-colors">
+                                        Learn more
+                                    </button>
+                                </Link>
+                            </div>
+                        </Card>
+                    ))}
                 </Slider>
             </div>
-
-            <div>
-
-            </div>
         </div>
-    )
+    );
 }
 
-export default Insingth
+export default Insingth;
