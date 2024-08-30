d03aad82fbeb6c632fa26196e0ece2842b52abdc
sandeep25sahu <sandeep.sahu2001@ssipmt.com>
2024-08-30 16:28:01 +0530

    tools update

--- Frontend/src/components/curosals/Insingth.tsx
+++ Frontend/src/components/curosals/Insingth.tsx
@@ -1,13 +1,14 @@
 
 import Slider from "react-slick";
 import { Card, CardBody, Image } from "@nextui-org/react";
+import { Link } from "@nextui-org/link";
 
 const list = [
-    {title: "Read Latest News",img:"src/components/graphics/Screenshot 2024-08-10 193221.png" },
-    {title: "Read Articals", img:"src/components/graphics/Screenshot 2024-08-10 193229.png" },
-    {title: "EMI calcutaror",img:"src/components/graphics/Screenshot 2024-08-10 193519.png"},
-    {title: "Area calcutaror",img:"src/components/graphics/Screenshot 2024-08-10 193519.png"},
-    {title: "Loan calcutaror",img:"src/components/graphics/Screenshot 2024-08-10 193519.png"},
+    {title: "Read Latest News",img:"src/components/graphics/Screenshot 2024-08-10 193221.png",url:"https://realty.economictimes.indiatimes.com/" },
+    {title: "Read Articals",img:"src/components/graphics/Screenshot 2024-08-10 193229.png" ,url:"https://www.lexisnexis.in/blogs/property-law-in-india/"},
+    {title: "EMI calcutaror",img:"src/components/graphics/Screenshot 2024-08-10 193519.png",url:"https://emicalculator.net/#google_vignette"},
+    {title: "Area calcutaror",img:"src/components/graphics/Screenshot 2024-08-10 193519.png",url:"https://www.thecalculatorsite.com/conversions/area.php"},
+    {title: "Loan eligibility",img:"src/components/graphics/Screenshot 2024-08-10 193519.png",url:"https://homeloans.sbi/calculators"},
   ];
 
 function Insingth() {
@@ -38,8 +39,6 @@ function Insingth() {
           className="card-hover h-60 w-40 bg-slate-100"
           key={index}
           shadow="sm"
-          
-          onPress={() => console.log("item pressed")}
         >
           <CardBody className="overflow-visible p-0 mx-5">
             <Image
@@ -48,6 +47,17 @@ function Insingth() {
             />
           </CardBody>
            <b className="text-black">{item.title}</b>
+
+           <Link
+            className="flex  items-center gap-1 "
+            color="foreground"
+            href={item.url}
+           >
+                <button className="bg-pink-600 border-none p-2 text-white rounded-2xl 
+                 hover:bg-pink-800 transition-colors mx-auto">
+          Learn more
+        </button>
+           </Link>
          
         </Card>
       ))}
